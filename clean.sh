300fb7729835c755e726f39d567cea875118057f
Sergey Shnatsel Davidoff <shnatsel@gmail.com>
2012-04-05 20:17:53 +0400

    added a check for root permissions to clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# check for root permissions
+if [ $(id -u) != 0 ]; then
+echo "You have to be root to clean up after root." > /dev/stderr
+exit 1
+fi
+
 lb clean
 rm -R cache
 rm -R auto/functions auto/scripts
