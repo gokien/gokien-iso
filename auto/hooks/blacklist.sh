88f83d0f81edcc94c6d0c493428d749a817b4fcb
Sergey Shnatsel Davidoff <shnatsel@gmail.com>
2012-04-22 00:49:46 +0400

    fixed blacklist leaving some package files behind

--- auto/hooks/blacklist.sh
+++ auto/hooks/blacklist.sh
@@ -3,7 +3,7 @@ apt-get install -q -f -y --allow-unauthenticated --no-install-recommends bzr
 bzr branch lp:~elementary-os/elementaryos/elementary-seeds.precise elementary-seeds.precise
 bzr branch lp:~elementary-os/elementaryos/platform.precise platform.precise
 for package in $(cat 'platform.precise/blacklist' 'elementary-seeds.precise/blacklist' | grep -v '#'); do
-apt-get -q -f -y autoremove "$package"
+apt-get -q -f -y autoremove --purge "$package"
 done
-apt-get -q -f -y autoremove bzr
+apt-get -q -f -y autoremove --purge bzr
 rm -R elementary-seeds.precise platform.precise
