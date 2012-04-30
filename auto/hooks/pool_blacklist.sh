3a4fdbbf806630fa14ec4f74080c810ddf8458b1
Ivo Nunes <netherblood@gmail.com>
2012-04-30 14:05:27 +0100

    Added a list of packages to the pool blacklist.

--- auto/hooks/pool_blacklist.sh
+++ auto/hooks/pool_blacklist.sh
@@ -5,7 +5,7 @@ for FILENAME in main/*/*
 
     do
         cd ${FILENAME%/*}
-        rm -f -R linux*
+        rm -f -R linux* gcc-* gtk+* python* binutils coreutils dpkg manpages ttf-* perl libsamplerate glib* nss alsa-utils udev dbus sqlite* cairo apt base-files base-passwd busybox bzip2 db debconf debianutils dmidecode e2fsprogs libx*
         cd ../..
     done
 
