416da0f0c487b5a2fdf86fbb8fb632488e7af7e9
Ivo Nunes <netherblood@gmail.com>
2012-04-30 14:45:47 +0100

    Better blacklisting system.

--- auto/hooks/pool_blacklist.sh
+++ auto/hooks/pool_blacklist.sh
@@ -4,9 +4,7 @@ cd binary/pool
 for FILENAME in main/*/*
 
     do
-        cd ${FILENAME%/*}
-        rm -f -R linux* gcc-* gtk+* python* binutils coreutils dpkg manpages ttf-* perl libsamplerate glib* nss alsa-utils udev dbus sqlite* cairo apt base-files base-passwd busybox bzip2 db debconf debianutils dmidecode e2fsprogs libx*
-        cd ../..
+        check=$(basename $FILENAME); [[ -n $(grep "^$check\$" ../../auto/pool) ]] && echo "" || rm -R $FILENAME
     done
 
 exit 0
