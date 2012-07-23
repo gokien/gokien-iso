05190a1a8f8642f62fa20ee24713901bb2ed6b28
Sergey Shnatsel Davidoff <shnatsel@gmail.com>
2012-07-23 22:29:19 +0400

    made prepare-sourceforge-upload.sh write filenames of renamed ISOs to disk so we can reuse the list for uploading

--- prepare-sourceforge-upload.sh
+++ prepare-sourceforge-upload.sh
@@ -2,6 +2,12 @@
 
 builddir=$( echo ./builds/* | tr ' ' '\n' | tail -n 1 )
 
+rm .latest_build_sourceforge_filenames
+
 for arch in i386 amd64; do
-[ -e "$builddir"/"$arch"/binary.iso ] && ./generate_zsync_metafile.sh $( ./rename_for_sourceforge.sh "$builddir"/"$arch"/binary.iso )
+    [ -e "$builddir"/"$arch"/binary.iso ] && ./rename_for_sourceforge.sh "$builddir"/"$arch"/binary.iso >> .latest_build_sourceforge_filenames
+done
+
+for file in $(cat .latest_build_sourceforge_filenames); do
+./generate_zsync_metafile.sh "$file"
 done
