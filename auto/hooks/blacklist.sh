25f90782f2b4f030198b8cf3aa44b5f19a213f40
Cody Garver <codygarver@gmail.com>
2012-08-13 20:23:47 -0500

    Use lightweight bzr checkout instead of slow branch.

--- auto/hooks/blacklist.sh
+++ auto/hooks/blacklist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get install -q -f -y --allow-unauthenticated --no-install-recommends bzr
-bzr branch lp:~elementary-os/elementaryos/elementary-seeds.precise elementary-seeds.precise
-bzr branch lp:~elementary-os/elementaryos/platform.precise platform.precise
+bzr co --lightweight lp:~elementary-os/elementaryos/elementary-seeds.precise elementary-seeds.precise
+bzr co --lightweight lp:~elementary-os/elementaryos/platform.precise platform.precise
 for package in $(cat 'platform.precise/blacklist' 'elementary-seeds.precise/blacklist' | grep -v '#'); do
 apt-get -q -f -y autoremove --purge "$package"
 done
