5cff827aa117b412685d33db8570abf59ac9656e
Ivo Nunes <netherblood@gmail.com>
2012-09-06 11:35:24 +0100

    Rewrote how the package pool is generated. The package pool now uses seeds. Added GPG signing to the pool.

--- auto/hooks/seeds.sh
+++ auto/hooks/seeds.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+function apt-download { wget $(apt-get install --reinstall --print-uris -qq $1 | cut -d"'" -f2 | grep "/${1}_"); }
+
 apt-get install -q -f -y --allow-unauthenticated --no-install-recommends bzr
 bzr branch lp:~elementary-os/elementaryos/elementary-seeds.precise elementary-seeds.precise
 bzr branch lp:~elementary-os/elementaryos/platform.precise platform.precise
@@ -6,4 +9,18 @@ for package in $(cat 'platform.precise/blacklist' 'elementary-seeds.precise/blac
 apt-get -q -f -y autoremove --purge "$package"
 done
 apt-get -q -f -y autoremove --purge bzr
-rm -R elementary-seeds.precise platform.precise
+
+mkdir pool
+cd pool
+
+while read -r line; do
+    if [[ $line == *"*"* ]]
+    then
+        line=${line%%#*}
+        line=${line%%[*}
+        line=${line/'*'/}
+        apt-download $line
+    fi
+done < "../elementary-seeds.precise/ship-live"
+
+rm -R ../elementary-seeds.precise ../platform.precise
